19e5c0754b65a85dd9b6ec867c23b9d724b0db3e
joyaty <joyaty@gmail.com>
2025-04-22 18:24:46 +0800

    * Add UIHotfixPanel

---
+++ Assets/GameScripts/GameBase/View/UIHotfixView.cs
@@ -0,0 +1,38 @@
+
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Joy.Base.UI
+{
+    /// <summary>
+    /// 资源更新UI控制脚本
+    /// </summary>
+    public class UIHotfixView : MonoBehaviour
+    {
+        /// <summary>
+        /// 背景图
+        /// </summary>
+        [SerializeField] private Image m_Background;
+
+        /// <summary>
+        /// 版本信息
+        /// </summary>
+        [SerializeField] private TextMeshProUGUI m_VersionCode;
+        
+        /// <summary>
+        /// 进度条
+        /// </summary>
+        [SerializeField] private Slider m_Progress;
+
+        /// <summary>
+        /// 进度提示信息
+        /// </summary>
+        [SerializeField] private TextMeshProUGUI m_Tips;
+
+        private void Start()
+        {
+            
+        }
+    }
+}
