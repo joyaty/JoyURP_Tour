925744f5954032e80ba09f8d7d8d0e3f7282a885
joyaty <joyaty@gmail.com>
2025-05-06 11:22:47 +0800

    Add Plugins: YIUI

---
+++ Assets/Plugins/YIUI/Plugins/YIUIMono/Effect/UIGrayscale.cs
@@ -0,0 +1,83 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace YIUIFramework
+{
+    [ExecuteInEditMode]
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(UIMaterialEffect))]
+    public sealed class UIGrayscale : MonoBehaviour
+    {
+        [SerializeField]
+        [Range(0, 255)]
+        private int grayscale = 0;
+
+        private UIMaterialEffect materialEffect;
+
+        public int GrayScale
+        {
+            get { return this.grayscale; }
+
+            set
+            {
+                if (this.grayscale != value)
+                {
+                    this.grayscale = value;
+                    this.Refresh();
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            this.Refresh();
+        }
+
+        private void OnDestroy()
+        {
+            if (this.materialEffect != null)
+            {
+                var key = this.materialEffect.MaterialKey;
+                key.GrayScale = 0;
+                this.materialEffect.MaterialKey = key;
+                this.materialEffect.MarkDirty();
+            }
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            this.Refresh();
+        }
+#endif
+
+        private void Refresh()
+        {
+#if UNITY_EDITOR
+            // TODO 2022 API升级调整，需要验证逻辑
+            // var prefabType = PrefabUtility.GetPrefabType(this.gameObject);
+            // if (prefabType == PrefabType.Prefab)
+            // {
+            //     return;
+            // }
+            var prefabAssetType = PrefabUtility.GetPrefabAssetType(this.gameObject);
+            if (prefabAssetType == PrefabAssetType.Regular)
+            {
+                return;
+            }
+#endif
+            if (this.materialEffect == null)
+            {
+                this.materialEffect =
+                    this.GetOrAddComponent<UIMaterialEffect>();
+            }
+
+            var key = this.materialEffect.MaterialKey;
+            key.GrayScale = (byte)this.grayscale;
+            this.materialEffect.MaterialKey = key;
+            this.materialEffect.MarkDirty();
+        }
+    }
+}
